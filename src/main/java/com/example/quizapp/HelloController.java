813c564e905babf6f6145f098df778e1f349eb6d
kuwahara <k.amrmkh@gmail.com>
2020-10-12 23:29:48 +0900

    HelloController.java作成 画面のページ遷移処理実装

---
+++ src/main/java/com/example/quizapp/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.quizapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "list";
+    }
+}
